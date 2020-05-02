bd685170ff8b1c539c02351ee584606dff21ea1f
dataoutputstream <mb150996@gmail.com>
2020-05-02 15:55:29 +0200

    Restore Overlay-stag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,12 +58,8 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := raphael_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/raphael
-ifneq ($(wildcard prebuilts/clang/host/linux-x86/clang-avalon),)
-  TARGET_KERNEL_CLANG_VERSION := avalon
-else
   # Find latest one
   TARGET_KERNEL_CLANG_VERSION := $(shell sh -c "find prebuilts/clang/host/linux-x86/ -type f -name clang -printf \"%T@ %Tc %p\n\" | sort -n | tail -n1 | sed 's/.*clang-\(.*\)\/bin.*/\1/'")
-endif
 
 
 # Platform
--- msmnile.mk
+++ msmnile.mk
@@ -39,7 +39,8 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-derpfest
+    $(LOCAL_PATH)/overlay-derpfest \
+    $(LOCAL_PATH)/overlay-stag
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
@@ -338,7 +339,7 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-rtti
 
 # PixelLiveWallpaper
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     PixelLiveWallpaperPrebuilt
 
 # Perf
